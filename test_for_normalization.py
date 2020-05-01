48c6c7e55f42511c44e68b5564f7fd90b4bf614a
nium9 <47532352+nium9@users.noreply.github.com>
2020-05-02 00:15:12 +0100

    Delete test_for_normalization.py

--- test_for_normalization.py
+++
@@ -1,5 +0,0 @@
-from pydub import AudioSegment, effects  
-
-rawsound = AudioSegment.from_file("Data/Data_for_stage_3/Sample_of_hello_internet.wav", "m4a")  
-normalizedsound = effects.normalize(rawsound)  
-normalizedsound.export("./output.wav", format="wav")
